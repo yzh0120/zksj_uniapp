fe9e9da97740bb2cd81128d5f80d350d0b7cfc98
yzh0120 <644193385@qq.com>
2023-02-22 14:18:51 +0800

    up

--- pages/subPacks/utils/http.js
+++ pages/subPacks/utils/http.js
@@ -1,4 +1,10 @@
 /*
+ * @Author: yz
+ * @Date: 2023-02-19 12:25:21
+ * @Description: 
+ * 
+ */
+/*
 uView 2.0版本 http请求封装
 https://blog.csdn.net/zhao949895887/article/details/121601533?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-0-121601533-blog-123294230.pc_relevant_recovery_v2&spm=1001.2101.3001.4242.1&utm_relevant_index=3
 
@@ -8,11 +14,15 @@ https://blog.csdn.net/Y1_1Y1_1y1_1y/article/details/123294230
 
 
 */
+
+import apiConfig from '@/pages/subPacks/utils/ip.js';
+
+
 module.exports = (vm) => {
 	// 初始化请求配置
 	uni.$u.http.setConfig((config) => {
 		/* config 为默认全局配置*/
-    config.baseURL = "https://szdbtest-wechatapp-trans.zksj.com.cn/api/proxy/"; /* 根域名 */
+    config.baseURL = apiConfig.url; /* 根域名 */
     config.timeout = 10000
 		return config
 	})
---
+++ pages/subPacks/utils/ip.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: yz
+ * @Date: 2023-02-22 14:14:10
+ * @Description: 
+ * 
+ */
+let apiConfig = {}
+
+apiConfig = {
+  url:"https://szdbtest-wechatapp-trans.zksj.com.cn/api/proxy/"
+}
+
+
+export default apiConfig;
